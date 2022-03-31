d766898e4dcf6e3c17ba84f588a823c647e41ce1
swiatek1995@gmail.com <swiatek1995@gmail.com>
2022-03-31 15:54:58 +0200

    saving in sign_up

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
@@ -9,6 +10,10 @@ def home(request):
 def sign_up(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
     else:
         form = RegisterForm()
     return render(request, 'registration/sign_up.html', {"form": form})
